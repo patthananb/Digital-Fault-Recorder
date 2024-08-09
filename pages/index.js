92cb0cb8670ea14eeb2e36ff8008000bcad9d038
patthananb <123183496+patthananb@users.noreply.github.com>
2024-08-09 22:57:36 +0700

    himan

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,20 @@
 import styles from "@/styles/Home.module.css"
+import * as React from "react";
+
+// 1. import `NextUIProvider` component
+import {NextUIProvider} from "@nextui-org/react";
 
 //home page
 export default function Home() {
   return (
     <>
-
+    <NextUIProvider>
       <div>
         <h1>
           this is home page
         </h1>
       </div>
-
+      </NextUIProvider>
     </>
   )
 }
